1bc4271534d1693e860a7df328d02900e24e3864
maxim <maxim.ryklis@spsmail.cuny.edu>
2016-10-12 12:52:39 -0400

    doc string

--- task_041.py
+++ task_041.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module measures input length and formats string."""
+
+
 MYINPUT = raw_input('Tell me a story! ')
 MAX_LENGTH = 80
 LONGSTR = 'short'
